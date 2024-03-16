f39abccfd083d22f1638ffe6d33cdceef8f06ffb
g8row <aliogu23@gmail.com>
2024-03-16 03:27:22 +0200

    initial FE BLE support

--- FE/src/navigation/AuthNavigationStack.tsx
+++ FE/src/navigation/AuthNavigationStack.tsx
@@ -1,8 +1,11 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
+import Play from "../screens/Play";
+
 
 export type AuthStackNavigationProps = {
-    home: undefined
+  home: undefined
+  play: undefined
 }
 
 const AuthStack = createBottomTabNavigator<AuthStackNavigationProps>();
@@ -10,7 +13,8 @@ const AuthStack = createBottomTabNavigator<AuthStackNavigationProps>();
 export default function AuthNavigationStack() {
   return (
     <AuthStack.Navigator>
-        <AuthStack.Screen name="home" component={Home}/>
+      <AuthStack.Screen name="home" component={Home} />
+      <AuthStack.Screen name="play" component={Play} />
     </AuthStack.Navigator>
   )
 }
